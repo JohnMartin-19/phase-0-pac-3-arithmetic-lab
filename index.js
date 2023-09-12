e764376d14dd67561f62e968af993681115a8eb6
John Mburu <johnnkonge2020@gmail.com>
2023-09-12 09:07:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+ let makeInt = '2'
+ function myF(){
+    parseInt('makeInt',10)
+  console.log(makeInt);
+ }
+ myF()
+
+ let preserveDecimal = '4.567'
+ function float(){
+    parseFloat("preserveDecimal");
+    console.log(preserveDecimal);
+ }
+ float()
